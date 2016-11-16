35594fa89af89935d6ca202fc1812fdbfa18c308
Alastair Rayner <alli0@me.com>
2016-11-16 16:18:55 +0000

    Players can choose what map and units to select

---
+++ Protoype V3/Lobby11/L2_Red10 - Lobby/Assets/Scripts/CameraSwitch.cs
@@ -0,0 +1,77 @@
+﻿//------------------------------------------------------------------------------------------------------
+// Author : Ross Perry 
+// Date : 14 / 11 / 2016  
+// Description : Manager to handle all functionality relating to camera switching
+// Features a singleton so that the camera location is always the same and functions to manage switching
+//-------------------------------------------------------------------------------------------------------
+
+
+using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class CameraSwitch : NetworkBehaviour
+{
+    //Defines the script as a singleton
+    public static CameraSwitch instance;
+
+    private bool allUnitsPlaced = false;
+
+    //Prevents problems if more than one instance of this singleton is used
+    void Awake ()
+    {
+        if (instance != null) 
+        {
+            Destroy(this);
+        }
+        else 
+        {
+            instance = this;
+        }
+
+    }
+
+    //Makes sure the camera is in the correct state when network clients are started
+    void OnStartClient() 
+    {
+        Camera.main.enabled = true; 
+    }
+
+    public void MoveCameraToSelectedUnit()
+    {
+        if (UnitManager.inst.GetContentsOfList(1).Count == 3 && UnitManager.inst.GetContentsOfList(2).Count == 3)  //Checks if all units have been placed
+        {
+            allUnitsPlaced = true;
+
+            if (allUnitsPlaced == true)
+            {
+                Camera.main.transform.position = UnitManager.inst.GetSelectedUnit().transform.position; //Transform the fps camera to the selected unit
+
+                Camera.main.transform.parent = UnitManager.inst.GetSelectedUnit().GetComponent<CharacterMovement>().dock.transform; //Parent to camera dock
+
+                Camera.main.transform.rotation = UnitManager.inst.GetSelectedUnit().GetComponent<CharacterMovement>().dock.transform.rotation; //Set new rotation
+
+                //firstPerson.transform.parent = null; //Unparent the camera from the old object
+                //To be added: disable and enable the setup script to coincide with the switch (enable movement etc)
+            } 
+            else
+            {
+                return;
+                //Send message to players "Not all units are placed"
+            }
+
+        }
+            
+    }
+
+    //W.I.P
+    void AssignNetworkHost()
+    {
+        //run NetworkManager.StartHost();  
+    }
+
+    void AssignNetworkClient()
+    {
+        //run NetworkManager.StartClient();
+    }
+}
---
+++ Protoype V3/Lobby11/L2_Red10 - Lobby/Assets/Scripts/Switch.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Switch : MonoBehaviour
+{
+    public static Switch inst; //Declare the variable
+    [SerializeField]
+    private int unitsLeftBeforeSwitch, playerTurn = 1;
+    private int defaultUnitAmounts;
+
+    [SerializeField]
+    private GameObject player;
+
+
+    private void Awake() //Initialise the variable
+    {
+        inst = this;
+    }
+
+    IEnumerator delayedCode()
+    {
+        yield return new WaitForSeconds(0.1f);
+
+        unitsLeftBeforeSwitch = UnitManager.inst.GetNumberOfPlayerUnits(1); //Get the amount of units found in the list
+        defaultUnitAmounts = unitsLeftBeforeSwitch;
+    }
+
+    private void FixedUpdate()
+    {
+        if (unitsLeftBeforeSwitch == 0) //If no units are left to control before switch, then switch
+        {
+            //Switch the player turn
+//            SetUnitsLeftBeforeSwitchToDefault();
+//            UnitManager.inst.SelectUnit(null);
+//            ResetEnergyOfAllUnits();
+//            if (playerTurn == 1)
+//            {
+//                playerTurn = 2;
+//               
+//            }
+//            else if (playerTurn == 2)
+//            {
+//                playerTurn = 1;
+//           
+//            }
+        }
+       
+    }
+
+    private void ResetEnergyOfAllUnits() //This will loop through the contents of the players in both lists for the players owned by each player and reset the energy values
+    {
+        foreach (GameObject player1 in UnitManager.inst.GetContentsOfList(1)) //Get contents of the list holding all of player 1s units and reset each energy value
+        {
+            UnitInfo infoScript = player1.GetComponent<UnitInfo>();
+            infoScript.ResetEnergy();
+        }
+        foreach (GameObject player2 in UnitManager.inst.GetContentsOfList(2)) //Get contents of the list holding all of player 2s units and reset each energy value
+        {
+            UnitInfo infoScript = player2.GetComponent<UnitInfo>();
+            infoScript.ResetEnergy();
+        }
+    }
+
+//    public Camera GetTacticalCamera() //Get the tactical camera without exposing variables
+//    {
+//        return tactCam;
+//    }
+
+    public void DecreaseUnitsLeftBeforeSwitch() //Decrease the amount of units before switch
+    {
+        unitsLeftBeforeSwitch--;
+    }
+
+    public void SetUnitsLeftBeforeSwitchToDefault() //Set units left before the switch back to default for the next player
+    {
+        unitsLeftBeforeSwitch = defaultUnitAmounts;
+    }
+
+    public int GetPlayerTurn() //When called will return the player whos turn it is
+    {
+        return playerTurn;
+    }
+        
+
+}
